4d5872540728037a7aec4dd413f2797745e9591a
bruno0409 <brunooliveiradesign@hotmail.com>
2025-11-16 12:18:24 -0300

    ajustando o touch do antes e depois

--- js/main.js
+++ js/main.js
@@ -40,23 +40,26 @@
     var $beforeAfterBar = $slider.find(".before-after-bar");
     var isMouseDown = false;
 
-    // Quando o usuário pressiona a barra de arraste
-    $beforeAfterBar.on("mousedown", function (e) {
-      isMouseDown = true;
-      $("body").css("user-select", "none"); // Impede a seleção de texto
-    });
-
-    // Quando o usuário move o mouse
-    $(document).on("mousemove", function (e) {
+    // Função para lidar com o movimento da barra de arraste
+    var moveSlider = function (e) {
       if (isMouseDown) {
+        // Calcula a posição do clique ou toque
         var sliderOffsetX = $slider.offset().left;
         var sliderWidth = $slider.width();
-        var offsetX = e.pageX - sliderOffsetX;
+        var offsetX;
+
+        if (e.type.startsWith("touch")) {
+          // Se for um evento de toque, pega a posição do toque
+          offsetX = e.originalEvent.touches[0].pageX - sliderOffsetX;
+        } else {
+          // Se for um evento de mouse, pega a posição do clique
+          offsetX = e.pageX - sliderOffsetX;
+        }
 
         // Limita o movimento da barra dentro dos limites do slider
         offsetX = Math.min(Math.max(0, offsetX), sliderWidth);
 
-        // Altera o "clip-path" da imagem "depois" com base no movimento da barra
+        // Atualiza o "clip-path" da imagem "depois" com base no movimento da barra
         $beforeAfterImg.css(
           "clip-path",
           "inset(0 " + (100 - (offsetX / sliderWidth) * 100) + "% 0 0)"
@@ -65,10 +68,23 @@
         // Atualiza a posição da barra de arraste
         $beforeAfterBar.css("left", offsetX + "px");
       }
+    };
+
+    // Quando o usuário pressiona a barra de arraste
+    $beforeAfterBar.on("mousedown touchstart", function (e) {
+      isMouseDown = true;
+      $("body").css("user-select", "none"); // Impede a seleção de texto
+      // Previne o comportamento padrão (evita o scroll em dispositivos móveis)
+      e.preventDefault();
     });
 
-    // Quando o usuário solta o mouse
-    $(document).on("mouseup", function () {
+    // Quando o usuário move o mouse ou toca
+    $(document).on("mousemove touchmove", function (e) {
+      moveSlider(e);
+    });
+
+    // Quando o usuário solta o mouse ou o toque
+    $(document).on("mouseup touchend", function () {
       isMouseDown = false;
       $("body").css("user-select", ""); // Restaura a seleção de texto
     });
